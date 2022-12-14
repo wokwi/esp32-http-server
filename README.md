a5f0de130a9670d22a36bbf364b735a1f5827542
Uri Shaked <uri@urishaked.com>
2022-12-14 19:55:18 +0200

    feat: initial release

---
+++ README.md
@@ -0,0 +1,19 @@
+# ESP32 HTTP Server Example
+
+ESP32 web server example: control 2 LEDs from a web page hosted on the ESP32.
+
+Use Wokwi for Visual Studio Code to simulate this project.
+
+## Building
+
+Install [Platform IO](https://platformio.org/), then run:
+
+```
+pio run
+```
+
+## Simulating
+
+Install the Wokwi extension for Visual Studio Code. Open the project, press "F1" and select "Wokwi: Start Simulator".
+
+Finally, open http://localhost:8180 to interact with the simulated webserver.
---
+++ src/esp32-http-server.ino
@@ -0,0 +1,109 @@
+/* ESP32 HTTP IoT Server Example for Wokwi.com
+
+  https://wokwi.com/arduino/projects/320964045035274834
+
+  When running it on Wokwi for VSCode, you can connect to the 
+  simulated ESP32 server by opening http://localhost:8180
+  in your browser. This is configured by wokwi.toml.
+*/
+
+#include <WiFi.h>
+#include <WiFiClient.h>
+#include <WebServer.h>
+#include <uri/UriBraces.h>
+
+#define WIFI_SSID "Wokwi-GUEST"
+#define WIFI_PASSWORD ""
+// Defining the WiFi channel speeds up the connection:
+#define WIFI_CHANNEL 6
+
+WebServer server(80);
+
+const int LED1 = 26;
+const int LED2 = 27;
+
+bool led1State = false;
+bool led2State = false;
+
+void sendHtml() {
+  String response = R"(
+    <!DOCTYPE html><html>
+      <head>
+        <title>ESP32 Web Server Demo</title>
+        <meta name="viewport" content="width=device-width, initial-scale=1">
+        <style>
+          html { font-family: sans-serif; text-align: center; }
+          body { display: inline-flex; flex-direction: column; }
+          h1 { margin-bottom: 1.2em; } 
+          h2 { margin: 0; }
+          div { display: grid; grid-template-columns: 1fr 1fr; grid-template-rows: auto auto; grid-auto-flow: column; grid-gap: 1em; }
+          .btn { background-color: #5B5; border: none; color: #fff; padding: 0.5em 1em;
+                 font-size: 2em; text-decoration: none }
+          .btn.OFF { background-color: #333; }
+        </style>
+      </head>
+            
+      <body>
+        <h1>ESP32 Web Server</h1>
+
+        <div>
+          <h2>LED 1</h2>
+          <a href="/toggle/1" class="btn LED1_TEXT">LED1_TEXT</a>
+          <h2>LED 2</h2>
+          <a href="/toggle/2" class="btn LED2_TEXT">LED2_TEXT</a>
+        </div>
+      </body>
+    </html>
+  )";
+  response.replace("LED1_TEXT", led1State ? "ON" : "OFF");
+  response.replace("LED2_TEXT", led2State ? "ON" : "OFF");
+  server.send(200, "text/html", response);
+}
+
+void setup(void) {
+  Serial.begin(115200);
+  pinMode(LED1, OUTPUT);
+  pinMode(LED2, OUTPUT);
+
+  WiFi.begin(WIFI_SSID, WIFI_PASSWORD, WIFI_CHANNEL);
+  Serial.print("Connecting to WiFi ");
+  Serial.print(WIFI_SSID);
+  // Wait for connection
+  while (WiFi.status() != WL_CONNECTED) {
+    delay(100);
+    Serial.print(".");
+  }
+  Serial.println(" Connected!");
+
+  Serial.print("IP address: ");
+  Serial.println(WiFi.localIP());
+
+  server.on("/", sendHtml);
+
+  server.on(UriBraces("/toggle/{}"), []() {
+    String led = server.pathArg(0);
+    Serial.print("Toggle LED #");
+    Serial.println(led);
+
+    switch (led.toInt()) {
+      case 1:
+        led1State = !led1State;
+        digitalWrite(LED1, led1State);
+        break;
+      case 2:
+        led2State = !led2State;
+        digitalWrite(LED2, led2State);
+        break;
+    }
+
+    sendHtml();
+  });
+
+  server.begin();
+  Serial.println("HTTP server started");
+}
+
+void loop(void) {
+  server.handleClient();
+  delay(2);
+}
